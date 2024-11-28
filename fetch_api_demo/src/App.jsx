599c948d6483f607f56755091e28d679f5ab8446
Vlad Prus <pruvladik@gmail.com>
2024-11-28 18:05:33 +0200

    add react query

--- fetch_api_demo/src/App.jsx
+++ fetch_api_demo/src/App.jsx
@@ -1,48 +1,42 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { CurrentPageContent } from "./components/CurrentPageContent.jsx";
 import { PageControls } from "./components/PageControls.jsx";
 import { Pagination } from "./components/PaginationNew.jsx";
+import { useQuery } from "@tanstack/react-query";
 
 const itemsPerPage = 5;
 const minPage = 1;
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const maxPage = useMemo(() => Math.ceil(data.length / itemsPerPage), [data]);
-
-  async function fetchData() {
-    try {
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/posts"
       );
-      const result = await response.json();
       if (!response.ok) {
         throw new Error("Помилка завантаження даних з серверу");
       }
-      setData(result);
-    } catch (error) {
-      setError(error.message);
-    }
-  }
+      return await response.json();
+    },
+  });
+
+  const maxPage = useMemo(
+    () => (data ? Math.ceil(data.length / itemsPerPage) : 0),
+    [data]
+  );
 
   const currentItemsMemo = useMemo(() => {
+    if (!data) return [];
     const indexEnd = currentPage * itemsPerPage;
     const indexStart = indexEnd - itemsPerPage;
     return data.slice(indexStart, indexEnd);
   }, [data, currentPage]);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetchData();
-    setIsLoading(false);
-  }, []);
-
-  if (isLoading) return <div>Завантаження... </div>;
-  if (error) return <div>{error}</div>;
+  if (isPending) return <div>Завантаження... </div>;
+  if (isError) return <div>{error.message}</div>;
 
   return (
     <>
