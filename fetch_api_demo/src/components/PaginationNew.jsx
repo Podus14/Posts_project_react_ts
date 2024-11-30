ebb4f9dcb792243c303647c0a78f67a6829d53e1
Podus14 <podus.leha@gmail.com>
2024-11-30 11:21:46 +0200

    Fix Pagination

--- fetch_api_demo/src/components/PaginationNew.jsx
+++ fetch_api_demo/src/components/PaginationNew.jsx
@@ -4,7 +4,7 @@ export const Pagination = ({
   maxPage,
   currentPage,
   setCurrentPage,
-  displayRange = 6,
+  displayRange = 5,
 }) => {
   const buttons = useMemo(() => {
     return Array.from({ length: maxPage }, (_, i) => i + 1);
@@ -19,25 +19,37 @@ export const Pagination = ({
 
     pageNumbers.push(1);
 
-    let rangeStart = Math.max(2, currentPage - Math.floor(displayRange / 2));
+    let rangeStart = Math.max(2, currentPage - Math.round(Math.floor(displayRange / 2)));
     let rangeEnd = Math.min(maxPage - 1, rangeStart + displayRange - 1);
 
-    if (rangeEnd - rangeStart < displayRange - 1) {
-      rangeStart = Math.max(2, rangeEnd - displayRange + 1);
+
+    if (rangeEnd === maxPage - 1) {
+      rangeStart = Math.max(2, rangeEnd - displayRange);
     }
 
-    if (rangeStart > 2) {
+    if (currentPage < 5) {
+      rangeEnd = rangeEnd + 1;
+    }
+    
+    if (rangeStart > 2 && currentPage !== displayRange) {
       pageNumbers.push("left-dots");
     }
+    if (currentPage === displayRange && rangeStart > 2) {
+      pageNumbers.push(2);
+    } 
 
     for (let i = rangeStart; i <= rangeEnd; i++) {
       pageNumbers.push(i);
     }
 
-    if (rangeEnd < maxPage - 1) {
+    if (rangeEnd === maxPage - 2) {
+      pageNumbers.push(maxPage-1);
+    } 
+    
+    if (rangeEnd < maxPage - 2) {
       pageNumbers.push("right-dots");
     }
-
+    
     if (maxPage !== rangeEnd) {
       pageNumbers.push(maxPage);
     }
@@ -45,14 +57,26 @@ export const Pagination = ({
     return pageNumbers;
   }, [buttons, currentPage, maxPage, displayRange]);
 
-  const handleEllipsisClick = (direction) => {
-    if (direction === "left") {
-      const newPage = Math.max(1, currentPage - displayRange);
+  const handleEllipsisClick = (direction) => {   
+          
+      if (currentPage > displayRange && currentPage < maxPage - 3){
+        if (direction === "left") {
+          const newPage = currentPage - displayRange + 2;
+          setCurrentPage(newPage);
+          return
+        }
+        const newPage = currentPage + displayRange - 2;
+        setCurrentPage(newPage);
+        return
+      }
+      if (direction === "left") {
+        const newPage = maxPage - displayRange - 2;
+        setCurrentPage(newPage);
+        return
+      }
+      const newPage = displayRange + 3;
       setCurrentPage(newPage);
-    } else {
-      const newPage = Math.min(maxPage, currentPage + displayRange);
-      setCurrentPage(newPage);
-    }
+      return
   };
 
   if (maxPage <= 1) return null;
@@ -77,7 +101,7 @@ export const Pagination = ({
                 }}
                 aria-label={`Jump ${
                   button === "left-dots" ? "backward" : "forward"
-                } by ${displayRange} pages`}
+                }`}
               >
                 ...
               </button>
